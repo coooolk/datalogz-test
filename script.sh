25ee5333c07cb7239e2b299089f31b4ac900e8c6
coooolk <coooolkpk@gmail.com>
2025-02-16 10:25:52 +0000

    using wget to only install app.py and not cloning the whole repo

--- script.sh
+++ script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-apt update -y && apt upgrade -y                        #makes the OS up-to-date
+apt update -y && apt upgrade -y                                                           #makes the OS up-to-date
 
-git clone https://github.com/coooolk/datalogz-test.git #cloned git repo
+wget https://raw.githubusercontent.com/coooolk/datalogz-test/refs/heads/master/app.py     #download app.py
 
-apt install -y python3 python3-pip python3-venv        # installed venv
+apt install -y python3 python3-pip python3-venv                                           # installed venv
 
-cd datalogz-test                                       #changed directory to git repo
+python3 -m venv .venv                                                                     #created a venv
+source .venv/bin/activate                                                                 #activated the venv
 
-python3 -m venv .venv                                  #created a venv
-source .venv/bin/activate                              #activated the venv
+pip install Flask                                                                         #installed flask
 
-pip install Flask                                      #installed flask
-
-nohup python3 app.py > flask.log 2>&1 &                #running app.py in bg using nohup comamnd
+nohup python3 app.py > flask.log 2>&1 &                                                   #running app.py in bg using nohup comamnd
 
 echo "Flask app started in the background."
