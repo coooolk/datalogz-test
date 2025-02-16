e33dcf85079dedf09e2692cb470d5d10a7862463
coooolk <coooolkpk@gmail.com>
2025-02-16 08:18:37 +0000

    updated scrip.sh to use python venv

--- script.sh
+++ script.sh
@@ -2,7 +2,9 @@
 apt update -y && apt upgrade -y
 git clone https://github.com/coooolk/datalogz-test.git
 apt install -y python3 python3-pip
-pip3 install Flask
 cd datalogz-test
-nohup python3 app.py &
+python3 -m venv .venv
+source .venv/bin/activate
+pip install Flask
+nohup python3 app.py > flask.log 2>&1 &
 echo "Flask app started in the background."
