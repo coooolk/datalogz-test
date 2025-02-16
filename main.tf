7928536be4855c8dbbff3deaa6ebdd3c6a6007ec
coooolk <coooolkpk@gmail.com>
2025-02-16 08:05:06 +0000

    added script.sh

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "ap-south-1"
 }
 
 resource "aws_security_group" "web_sg" {
@@ -7,9 +7,16 @@ resource "aws_security_group" "web_sg" {
   description = "Allow HTTP traffic on port 80"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -22,9 +29,9 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0cb91c7de36eed2cb" # Ubuntu server 24.04 AMI
-  instance_type          = "t3.small"
-  #key_name               = "exercise-key"
+  ami                    = "ami-00bb6a80f01f03502" # Ubuntu server 24.04 AMI
+  instance_type          = "t2.micro"
+  key_name               = "default-aws-key-pair"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = file("script.sh")
@@ -37,7 +44,7 @@ resource "aws_instance" "web" {
 data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
-    values = ["vpc-00acc805e3b63b146"]
+    values = ["vpc-027c5aa72f29ede16"]
   }
 }
 
@@ -46,14 +53,14 @@ resource "aws_lb" "app_lb" {
   name               = "app-lb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = ["subnet-0493780a7d4cd5613", "subnet-0563d2e43fe59896f"] # Example subnets
+  subnets            = ["subnet-0f73f26d3e8a041a7", "subnet-0d7ae98be33c81945", "subnet-05bf5b97322f4d0a6"] # Example subnets
 }
 
 resource "aws_lb_target_group" "tg" {
   name     = "flask-tg"
   port     = 5000
   protocol = "HTTP"
-  vpc_id   = "vpc-00acc805e3b63b146"
+  vpc_id   = "vpc-027c5aa72f29ede16"
 
   health_check {
     path                = "/health"
@@ -81,4 +88,15 @@ resource "aws_lb_target_group_attachment" "tg_attachment" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.web.id
   port             = 5000
+}
+
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
 }
--- script.sh
+++ script.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-apt update && apt upgrade
+apt update -y && apt upgrade -y
+git clone https://github.com/coooolk/datalogz-test.git
+apt install -y python3 python3-pip
+pip3 install Flask
+cd datalogz-test
+nohup python3 app.py &
+echo "Flask app started in the background."
