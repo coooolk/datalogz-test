88022b70dca410113f6cdb94a3301c3039460cb9
coooolk <coooolkpk@gmail.com>
2025-02-19 09:13:54 +0000

    updated scripts comments

--- script.sh
+++ script.sh
@@ -4,13 +4,13 @@ apt update -y && apt upgrade -y                                                 
 
 wget https://raw.githubusercontent.com/coooolk/datalogz-test/refs/heads/master/app.py     #download app.py
 
-apt install -y python3 python3-pip python3-venv                                           # installed venv
+apt install -y python3 python3-pip python3-venv                                           #install venv
 
-python3 -m venv .venv                                                                     #created a venv
-source .venv/bin/activate                                                                 #activated the venv
+python3 -m venv .venv                                                                     #create a venv
+source .venv/bin/activate                                                                 #activat the venv
 
-pip install Flask                                                                         #installed flask
+pip install Flask                                                                         #install flask
 
-nohup python3 app.py > flask.log 2>&1 &                                                   #running app.py in bg using nohup comamnd
+nohup python3 app.py > flask.log 2>&1 &                                                   #run app.py in bg using nohup comamnd
 
 echo "Flask app started in the background."
